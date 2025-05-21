bd46b14c094e6b2f2d853344c16fd28ee80c1112
utkbkts <utkbktss5@gmail.com>
2025-05-21 23:27:08 +0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevJourney",
+  description: "Full Stack Developer Journey, Frontend, Backend, Full Stack",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
