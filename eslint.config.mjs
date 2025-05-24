1eb009f6329980be0ace5c645dc2988ebefc6855
utkbkts <utkbktss5@gmail.com>
2025-05-24 14:52:01 +0300

    fixed:eslint vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,10 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-      ],
+      "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
