e7d5571aad62b8c74a0960543fa19a4e12964484
utkbkts <utkbktss5@gmail.com>
2025-05-25 13:47:44 +0300

    feat:clerk webhooks

---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,43 @@
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req);
+
+    const { id } = evt.data;
+    const eventType = evt.type;
+    console.log(
+      `Received webhook with ID ${id} and event type of ${eventType}`
+    );
+    console.log("Webhook payload:", evt.data);
+
+    if (evt.type === "user.created") {
+      console.log("userId:", evt.data.id);
+    }
+    if (eventType === "user.created" || eventType === "user.updated") {
+      const {
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username,
+      } = evt?.data;
+      console.log(
+        "🚀 ~ POST ~ id, first_name, last_name, image_url, email_addresses, username :",
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username
+      );
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+}
