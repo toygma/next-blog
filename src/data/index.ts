e01c8d6380e682fe70a756fdd50bbec01a0e1390
utkbkts <utkbktss5@gmail.com>
2025-05-27 16:15:28 +0300

    feat:seeding prisma added

---
+++ src/data/index.ts
@@ -0,0 +1,74 @@
+export const links = [
+  {
+    postType: "Project",
+    title: "title",
+    content: "content",
+    featuredImage:
+      "https://res.cloudinary.com/dpojqk4ty/image/upload/v1748350235/website/b1q5wxlzyd4c0bvs3f4y.webp",
+    authorId: "user_2xbX34lyRBv3t3zoW4N57Vcv9sF",
+    categories: ["React", "Next.js", "Node.js"],
+  },
+  {
+    postType: "Project",
+    title: "title",
+    content: "content",
+    featuredImage:
+      "https://res.cloudinary.com/dpojqk4ty/image/upload/v1748350235/website/b1q5wxlzyd4c0bvs3f4y.webp",
+    authorId: "user_2xbX34lyRBv3t3zoW4N57Vcv9sF",
+    categories: ["React", "Next.js", "Node.js"],
+  },
+  {
+    postType: "Project",
+    title: "title",
+    content: "content",
+    featuredImage:
+      "https://res.cloudinary.com/dpojqk4ty/image/upload/v1748350235/website/b1q5wxlzyd4c0bvs3f4y.webp",
+    authorId: "user_2xbX34lyRBv3t3zoW4N57Vcv9sF",
+    categories: ["React", "Next.js", "Node.js"],
+  },
+  {
+    postType: "Project",
+    title: "title",
+    content: "content",
+    featuredImage:
+      "https://res.cloudinary.com/dpojqk4ty/image/upload/v1748350235/website/b1q5wxlzyd4c0bvs3f4y.webp",
+    authorId: "user_2xbX34lyRBv3t3zoW4N57Vcv9sF",
+    categories: ["React", "Next.js", "Node.js"],
+  },
+  {
+    postType: "Blog",
+    title: "title",
+    content: "content",
+    featuredImage:
+      "https://res.cloudinary.com/dpojqk4ty/image/upload/v1748350235/website/b1q5wxlzyd4c0bvs3f4y.webp",
+    authorId: "user_2xbX34lyRBv3t3zoW4N57Vcv9sF",
+    categories: ["React", "Next.js", "Node.js"],
+  },
+  {
+    postType: "Blog",
+    title: "title",
+    content: "content",
+    featuredImage:
+      "https://res.cloudinary.com/dpojqk4ty/image/upload/v1748350235/website/b1q5wxlzyd4c0bvs3f4y.webp",
+    authorId: "user_2xbX34lyRBv3t3zoW4N57Vcv9sF",
+    categories: ["React", "Next.js", "Node.js"],
+  },
+  {
+    postType: "Blog",
+    title: "title",
+    content: "content",
+    featuredImage:
+      "https://res.cloudinary.com/dpojqk4ty/image/upload/v1748350235/website/b1q5wxlzyd4c0bvs3f4y.webp",
+    authorId: "user_2xbX34lyRBv3t3zoW4N57Vcv9sF",
+    categories: ["React", "Next.js", "Node.js"],
+  },
+  {
+    postType: "Blog",
+    title: "title",
+    content: "content",
+    featuredImage:
+      "https://res.cloudinary.com/dpojqk4ty/image/upload/v1748350235/website/b1q5wxlzyd4c0bvs3f4y.webp",
+    authorId: "user_2xbX34lyRBv3t3zoW4N57Vcv9sF",
+    categories: ["React", "Next.js", "Node.js"],
+  },
+];
