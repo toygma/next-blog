1c2e8eb952fb914454bd877c9ba853513b04fa28
toygma <toygunbektasoglu@gmail.com>
2025-09-20 12:22:08 +0300

    fixed: vercel deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prisma = new PrismaClient().$extends(withAccelerate())
